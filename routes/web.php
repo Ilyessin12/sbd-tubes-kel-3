78ce34e9c85843fbc97272718166cf553a701396
Lyan <lyanacw@upi.edu>
2024-06-10 23:14:38 +0700

    adding admin middlewares and change the default route when the user has logged in so that its redirected to "/"

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+//Bagian Controller Frontend
+use App\Http\Controllers\Frontend\HomeController;
+
+//Bagian Controller Admin
+use App\Http\Controllers\Admin\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +19,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('frontend.welcome');
 });
+*/
+
+//Routing untuk Bagian Frontend
+Route::get('/', [HomeController::class, 'index']);
+
+//Routing untuk Bagian Admin
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->middleware('admin');
+//untuk redirect ke dashboard jika ada yang mengetik "/admin" di url
+Route::redirect('/admin', '/admin/dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -30,6 +45,7 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+/*
 // Yg di bawah ini temporary buat testing, hapus aja nanti
 Route::get('/form-booking', function(){
     return view('frontend.create-booking');
@@ -37,3 +53,4 @@ Route::get('/form-booking', function(){
 Route::get('/tes-dashboard', function(){
     return view('admin.dashboard');
 });
+*/
