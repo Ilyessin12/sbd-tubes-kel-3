d680d71f993e48c5a8543f789d20570bae4f2717
VarrellRIM <varrellrim04@upi.edu>
2024-06-11 00:07:11 +0700

    commit testing

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use App\Http\Controllers\Admin\DashboardController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-|
 */
 
 /*
