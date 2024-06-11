6fb422a23ac871a3d8a32b6e8afb722495097037
VarrellRIM <varrellrim04@upi.edu>
2024-06-11 21:37:04 +0700

    controller untuk admin customer dan admin voucher

--- app/Http/Controllers/Admin/AdminCustomerController.php
+++ app/Http/Controllers/Admin/AdminCustomerController.php
@@ -10,26 +10,18 @@ class AdminCustomerController extends Controller
 {
     public function index()
     {
-        //kita ambil dulu data yang ada di database untuk tabel customer, abis itu kita kasih data nya ke view
-        // $customers = DB::select('SELECT * FROM customer');
-
-        //nanti return viewnya buat tampilan utama customer sekaligus sama data customernya, usahain pisahin pake folder customer
-        // return view('admin.customer.index', compact('customers'));
+        $customers = DB::select('SELECT * FROM customer');
+        
+        return view('admin.customer.index', compact('customers'));
     }
 
     public function create()
     {
-        //nanti return viewnya buat form tambah customer, usahain pisahin pake folder customer
-        // return view('admin.customer.create');
+        return view('admin.customer.create');
     }
     public function store(Request $request)
     {
-        //nanti disini buat validasi inputan, kalo validasi gagal balik ke form tambah customer
-        //kalo validasi sukses, simpen ke database terus balik ke halaman utama customer
 
-        // contoh validasi
-
-       /*
         $request->validate([
             'nama_customer' => 'required|string|max:255',
             'email_customer' => 'required|string|email|max:255|unique:customer',
@@ -42,11 +34,8 @@ class AdminCustomerController extends Controller
             'kuota_member' => 'required|integer',
         ]);
 
-        // Hash the password
         $hashedPassword = \Hash::make($request->password);
 
-        //abis di validasi, kita baru jalanin sql insert into ke database
-        /*
         DB::insert('INSERT INTO customer (nama_customer, email_customer, telp_customer, password, status_member, tanggal_mulai, tanggal_selesai, role, kuota_member)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [
             $request->nama_customer,
@@ -59,31 +48,21 @@ class AdminCustomerController extends Controller
             $request->role,
             $request->kuota_member,
         ]);
-        */
 
-        // abis insert, balik ke halaman utama customer (index page)
-        // return redirect()->route('admin.customer.index');
+        return redirect()->route('admin.customer.index');
 
 
     }
 
     public function edit($id)
     {
-        //ambil data customer yang mau diedit, terus kasih ke view
-        // $customer = DB::select('SELECT * FROM customer WHERE id_customer = ?', [$id])[0];
+        $customer = DB::select('SELECT * FROM customer WHERE id_customer = ?', [$id])[0];
 
-        //nanti return viewnya buat form edit customer, usahain pisahin pake folder customer
-        // return view('admin.customer.edit');
+        return view('admin.customer.edit');
     }
 
     public function update(Request $request, $id)
     {
-        //nanti disini buat validasi inputan, kalo validasi gagal balik ke form edit customer
-        //kalo validasi sukses, update database terus balik ke halaman utama customer
-
-        // contoh validasi
-
-        /*
         $request->validate([
             'nama_customer' => 'required|string|max:255',
             'email_customer' => 'required|string|email|max:255|unique:customer,email_customer,' . $id . ',id_customer',
@@ -96,11 +75,8 @@ class AdminCustomerController extends Controller
             'kuota_member' => 'required|integer',
         ]);
 
-        // Hash the password
         $hashedPassword = \Hash::make($request->password);
 
-        //abis di validasi, kita baru jalanin sql update ke database
-        /*
         DB::update('UPDATE customer SET nama_customer = ?, email_customer = ?, telp_customer = ?, password = ?, status_member = ?, tanggal_mulai = ?, tanggal_selesai = ?, role = ?, kuota_member = ?
         WHERE id_customer = ?', [
             $request->nama_customer,
@@ -114,20 +90,15 @@ class AdminCustomerController extends Controller
             $request->kuota_member,
             $id,
         ]);
-        */
 
-        // abis update, balik ke halaman utama customer (index page)
-        // return redirect()->route('admin.customer.index');
+
+        return redirect()->route('admin.customer.index');
     }
 
     public function destroy($id)
     {
-        //kalo delete, langsung delete dari database terus balik ke halaman utama customer
+        DB::delete('DELETE FROM customer WHERE id_customer = ?', [$id]);
 
-        // DB::delete('DELETE FROM customer WHERE id_customer = ?', [$id]);
-
-        // abis delete, balik ke halaman utama customer (index page)
-
-        // return redirect()->route('admin.customer.index');
+        return redirect()->route('admin.customer.index');
     }
 }
--- app/Http/Controllers/Admin/AdminVoucherController.php
+++ app/Http/Controllers/Admin/AdminVoucherController.php
@@ -7,5 +7,71 @@ use Illuminate\Http\Request;
 
 class AdminVoucherController extends Controller
 {
-    //
+    public function index()
+    {
+        $vouchers = DB::select('SELECT * FROM voucher');
+
+        return view('admin.voucher.index', compact('voucher'));
+    }
+
+    public function create()
+    {
+        return view('admin.voucher.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_voucher' => 'required|string|max:255',
+            'persentase_diskon' => 'required|integer',
+            'tanggal_mulai' => 'nullable|date|after_or_equal:today',
+            'tanggal_kadaluarsa' => 'nullable|date|after_or_equal:today',
+        ]);
+
+        DB::insert('INSERT INTO voucher (nama_voucher, persentase_diskon, tanggal_mulai, tanggal_selesai)
+        VALUES (?, ?, ?, ?, ?)', [
+            $request->nama_voucher,
+            $request->persentase_diskon,
+            $request->tanggal_mulai,
+            $request->tanggal_selesai,
+        ]);
+
+        return redirect()->route('admin.voucher.index');
+    }
+
+    public function edit($id)
+    {
+        $voucher = DB::select('SELECT * FROM voucher WHERE id_voucher = ?', [$id])[0];
+
+        return view('admin.customer.edit');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_voucher' => 'required|string|max:255',
+            'persentase_diskon' => 'required|integer',
+            'tanggal_mulai' => 'nullable|date|after_or_equal:today',
+            'tanggal_kadaluarsa' => 'nullable|date|after_or_equal:today',
+        ]);
+
+        DB::update('UPDATE voucher SET nama_voucher = ?, persentase_diskon = ?, telp_customer = ?, telp_customer = ?, tanggal_mulai = ?, tanggal_selesai = ?
+        WHERE id_voucher = ?', [
+            $request->nama_voucher,
+            $request->persentase_diskon,
+            $request->telp_customer,
+            $request->tanggal_mulai,
+            $request->tanggal_selesai,
+        ]);
+
+        return redirect()->route('admin.voucher.index');
+    }
+
+    public function destroy($id)
+    {
+
+        DB::delete('DELETE FROM voucher WHERE id_voucher = ?', [$id]);
+
+        return redirect()->route('admin.voucher.index');
+    }
 }
